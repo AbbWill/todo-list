82d5ea3a8a5084a90bba92830c830e7ee1b6f2cf
AbbWill <derfvc.xx@gmail.com>
2021-09-20 22:44:49 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 
 const exphbs = require('express-handlebars')
 
+const bodyParser = require('body-parser')
+
 const Todo = require('./models/todo')
 
 const app = express()
@@ -22,11 +24,31 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true}))
+
 app.get('/', (req, res) => {
-  Todo.find()
-    .lean()
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.error(error))
+Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
+})
+
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  
+  // const todo = new Todo({ name })
+  // return todo.save()
+  // .then(() => res.redirect('/'))
+  // .catch(error => console.log(error))
+
+  return Todo.create({ name })
+  .then(() => res.redirect('/'))
+  .catch(error => console.log(error))
 
 })
 
