0226a23fe903f1cc7c03753afaae81ddb3df7ed9
AbbWill <derfvc.xx@gmail.com>
2021-09-20 17:42:23 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+const e = require('express')
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongod connected!')
+})
+
 
 app.get('/', (req, res) => {
   res.send('hello world')
